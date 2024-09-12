27f2315eb91faba61b6b0214c8df2d301af7b3f5
PrakharSinghOnGit <singhprakhar1209@gmail.com>
2024-09-13 04:05:49 +0530

    yp

--- server.js
+++ server.js
@@ -5,17 +5,26 @@ const newsapi = new NewsAPI("252ac6baf82444b199607c797e361e4e");
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
+async function getNews() {
+  const news = await newsapi.v2.topHeadlines({
+    q: "a",
+    language: "en",
+    sortBy: "relevancy",
+  });
+  return news.articles;
+}
 
 app.prepare().then(() => {
   const server = express();
 
   server.get("/api/news", async (req, res) => {
-    const news = await newsapi.v2.topHeadlines({
-      q: "a",
-      language: "en",
-      sortBy: "relevancy",
-    });
-    res.send(news.articles);
+    res.send(await getNews());
+  });
+
+  server.get("/api/summ", async (req, res) => {
+    const news = await getNews();
+
+    res.send(article);
   });
 
   // Handle all other Next.js pages
